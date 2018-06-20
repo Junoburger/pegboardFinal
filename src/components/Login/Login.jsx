405e404c71cf0762ff9331d0d2fa1150cc2d9415
JunoJulius <yunoyulius@gmail.com>
2018-06-20 21:58:27 +0200

    style changes-welcomescreen-creating a form for validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 import {Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
 import {LogUser} from '../../actions/users'
-import { Textbox } from 'react-inputs-validation';
 
 
 
@@ -18,7 +17,8 @@ class Login extends React.Component {
       }
   }
   onEmailChange = (event) => {
-    console.log(this.state.LoginMail)
+    // console.log(this.state.LoginMail)
+    console.log(event.target.value)
     this.setState({LoginMail: event.target.value})
   }
   onPasswordChange = (event) => {
@@ -44,14 +44,14 @@ if(userLogin.length === 1){
   }
   render() {
 const onEmailChange = this.onEmailChange
-    return (<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+    return (<article className="br4 ba b--black-10 mv4 w-200 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
 
         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
           <legend className="f1 fw6 ph0 mh0">Sign In</legend>
           <div className="mt3">
-            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+            <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
+            <input className="b br3 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               type="email"
                name="email-address"
                id="email-address"
@@ -59,8 +59,8 @@ const onEmailChange = this.onEmailChange
 
           </div>
           <div className="mv3">
-            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+            <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
+            <input className="b br3 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
               type="password"
                name="password"
                 id="password"
@@ -68,15 +68,16 @@ const onEmailChange = this.onEmailChange
           </div>
         </fieldset>
         <div className="">
-          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+          <input className="b br3 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4 dib"
             type="submit"
             value="Sign in"
             onClick={this.onSubmitLogin}
             />
         </div>
         <div className="mv3">
-          <Link className="link underline blue hover-orange" to="/Register">Not A Member?</Link><br/><br/>
-          <Link  to="/Register"><input  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+          <span className="f4">Or</span><br/><br/>
+          <Link  to="/Register"><input
+             className="b br3 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4 dib"
             name="Register-redirect"
             id="Register-redirect"
              type="button" value="Register"/></Link>
