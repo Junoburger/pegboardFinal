10575d738c6aababfde5ba8b4d9f7fe2cce62fdb
JunoJulius <yunoyulius@gmail.com>
2018-06-20 20:12:42 +0200

    working on Validation with react Textbox styling components further

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import {Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
 import {LogUser} from '../../actions/users'
+// import { Textbox } from 'react-inputs-validation';
 
 
 
@@ -42,7 +43,7 @@ if(userLogin.length === 1){
 
   }
   render() {
-
+const onEmailChange = this.onEmailChange
     return (<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
 
@@ -55,6 +56,19 @@ if(userLogin.length === 1){
                name="email-address"
                id="email-address"
              onChange={this.onEmailChange}/>
+             {/* <Textbox
+  tabIndex="1" //Optional.[String or Number].Default: -1.
+  id={'email-address'} //Optional.[String].Default: "".  Input ID.
+  name="email-address" //Optional.[String].Default: "". Input name.
+  type="email" //Optional.[String].Default: "text". Input type [text, password, number].
+  // value={onEmailChange} //Optional.[String].Default: "".
+  placeholder="Place your email here ^-^" //Optional.[String].Default: "".
+  // onChange={name => this.setState({onEmailChange})} //Required.[Func].Default: () => {}. Will return the value.
+  onBlur={() => {}} //Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
+
+  onChange={this.onEmailChange}
+/> */}
+
           </div>
           <div className="mv3">
             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
