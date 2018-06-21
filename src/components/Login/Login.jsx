ce258677af85d68bb7b557e10e9226f10a018b7b
JunoJulius <yunoyulius@gmail.com>
2018-06-21 12:10:48 +0200

    Latest Logo Update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,7 +25,6 @@ class Login extends React.Component {
   onPasswordChange = (event) => {
     this.setState({LoginPassword: event.target.value})
   }
-
   onSubmitLogin = (e) => {
     e.preventDefault()
     console.log(this.state.LoginMail)
@@ -47,14 +46,10 @@ if(userLogin.length === 1){
 
     return (<div className="shape">
       <img src={logo} alt="PegBoardLogo" className="Logo"/>
-
       <article className="br4 b--black-10 mv4 w-200 w-50-m w-25-l mw6 sqrComp center">
       <main className="pa4 black-80">
-
         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-
           <legend style={{backgroundColor:'white', padding:'20px',borderRadius: '20px'}} className="f1 fw6 ph0 mh0">Sign In</legend>
-
           <div className="mt3">
             <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
             <input className="b br3 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
@@ -62,7 +57,6 @@ if(userLogin.length === 1){
                name="email-address"
                id="email-address"
              onChange={this.onEmailChange}/>
-
           </div>
           <div className="mv3">
             <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
