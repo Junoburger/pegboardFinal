b59f0200ccad2ce2e1cb3efa6075e674362ffcc9
tasos <tasos.krikonis@gmail.com>
2018-06-22 12:42:08 +0200

    final

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -10,11 +10,11 @@ class Swiper extends Component {
   state = {
     index: 0,
     flag: false,
-    holder:[]
+    holder: []
 
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const category = this.props.matchParams.category
 
     let type = this.props.matchParams.type
@@ -25,59 +25,56 @@ class Swiper extends Component {
       type = 'freelancer'
     }
 
-    this.setState({
-      holder: this.props.user.posts[category][type].filter((x) => x.posterId !== this.props.user.logUser)
+    this.setState({holder: this.props.user.posts[category][type].filter((x) => x.posterId !== this.props.user.logUser)
     })
 
   }
 
   increment = (index) => {
-      this.setState({
-        index: index
-      })
+    this.setState({index: index})
   }
 
   decline = () => {
     console.log(this.state.holder)
     let arr = [...this.state.holder]
-    const arr2 = ['a','s','f']
+    const arr2 = ['a', 's', 'f']
     console.log(arr2)
-    console.log(arr2.splice(1,1))
-
+    console.log(arr2.splice(1, 1))
 
   }
 
-makerequets = (holder)=> {
-  console.log(this.state.index)
-  this.props.makeRequest({
-    requesterId: this.props.user.logUser,
-    ...holder[this.state.index]
-  })
-}
-
+  makerequets = (holder) => {
+    console.log(this.state.index)
+    this.props.makeRequest({
+      requesterId: this.props.user.logUser,
+      ...holder[this.state.index]
+    })
+  }
 
   render() {
-
-
 
     if (this.state.flag) {
       return <Redirect to={'/home'}></Redirect>
     }
 
+    return (<div className="container">
 
-    return (<div className="main mw5 mw7-ns center bg-light-gray pa3 ph5-ns measure-narrow">
+       {
+        <div className="card">
 
-      <div>
+            <Slide users={this.props.users} data={this.state.holder} increment={this.increment}/>
+          </div>
 
-        {
-          <div className="fl w-60 ba">
-              <Slide users={this.props.users} data={this.state.holder} increment={this.increment}/> {/* <h1 key={holder[this.state.index].posterId}>{holder[this.state.index].postBody.description}</h1> */}
-              <button className="dislike-button fl w-30 bg-washed-red br3 grow" >Dislike</button>
-              <button className="like-button fl w-30 bg-washed-green br3 grow" onClick={() => this.makerequets(this.state.holder)} >Like</button>
-            </div>
-        }
-
+      }
+      <div className="buttons">
+        <button className="dislike-button">
+          ✗
+        </button>
+        <button className="like-button" onClick={() => this.makerequets(this.state.holder)}>
+          ✔
+        </button>
       </div>
+
     </div>)
   }
 }
